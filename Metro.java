6f6f16612fbd015f1e8150462b55cc782d7bd6b6
vivek020598 <vivekm.xworkz@gmail.com>
2024-02-18 23:21:25 -0800

    hello this is my first commit

---
+++ Metro.java
@@ -0,0 +1,16 @@
+class Metro{
+public static void main(String train[]){
+
+String metroName = "namma metro";
+boolean isGovtOriented = true;
+String source = "Yeshwanthpura";
+String destination = "Rajajinagar";
+
+
+System.out.println("metro name is:" + metroName);
+System.out.println("is metro government oriented :" + isGovtOriented);
+System.out.println("metro source:" + source);
+System.out.println("metro destination:" + destination);
+
+}
+}
